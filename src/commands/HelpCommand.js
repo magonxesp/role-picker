5676bc8c459cf06810aae2375eafcc8d74105c31
MagonxESP <janma.360@gmail.com>
2020-09-12 17:05:41 +0200

    Help command

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -1,18 +1,37 @@
-import { BotCommand } from "../bot";
+import { BotCommand, bot } from "../bot";
+import {rolesNames} from "../helpers";
 
 
 export default class HelpCommand extends BotCommand {
 
-    get description() {
+    get name() {
         return "help";
     }
 
-    get name() {
+    get description() {
         return "Listado de comandos";
     }
 
     async run(message, args) {
-        return Promise.resolve(undefined);
+        let help = ``;
+
+        for (let command of bot.commands) {
+            if (command.command.name !== this.name) {
+                help += `**${command.fullName}** - ${command.command.description}\n`;
+            }
+        }
+
+        await message.channel.send({
+            embed: {
+                color: 16777215,
+                fields: [
+                    {
+                        name: "Lista de comandos",
+                        value: help
+                    }
+                ]
+            }
+        });
     }
 
 }
